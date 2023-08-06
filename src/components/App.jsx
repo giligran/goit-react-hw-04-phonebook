c2804938fbe2c80d1330cc73e55d7369a7833a2e
giligran <kuprykovrodion@gmail.com>
2023-08-06 14:40:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,15 @@ import useLocalStorage from './utils/hooks';
 import { ContactForm, Filter, ContactList } from './Phonebook';
 import { Wrapper, ContactsContaier } from './Phonebook/Phonebook.styled';
 
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
-  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [contacts, setContacts] = useLocalStorage('contacts', defaultContacts);
   const [filter, setFilter] = useState('');
 
   const handleSubmit = data => {
